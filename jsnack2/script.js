464e9797345c2b961112d574028d9d8eeb121247
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-14 16:57:56 +0100

    BONUS per jsnack 3

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,16 +4,16 @@ let word_2 = prompt("Digiti la seconda parola");
 let length_1 = word_1.length;
 let length_2 = word_2.length;
 
-if(length_1 > length_2){
+if (length_1 > length_2) {
 
     console.log(
-    `
+        `
         La parola più corta è: ${word_2}
     
         La parola più lunga è: ${word_1}
 
     `);
-} else if (length_1 < length_2){
+} else if (length_1 < length_2) {
 
     console.log(
         `
@@ -24,6 +24,6 @@ if(length_1 > length_2){
         `);
 } else {
 
-    console.log("Le parole da lei scelte ("+ word_1 +" e "+ word_2 +") sono di uguale lunghezza!");
+    console.log("Le parole da lei scelte (" + word_1 + " e " + word_2 + ") sono di uguale lunghezza!");
 
 };
