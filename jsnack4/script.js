2f0d92b43159df7e47c5b05e92ac74b4257abdb8
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-14 17:12:53 +0100

    BONUS per jsnack 4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,9 +1,11 @@
-let guest_list = ["Jay", "Daisy", "Myrtle", "Jordan", "Henry", "Nick", "Tom", "Meyer", "George"];
+let guest_list = ["jay", "daisy", "myrtle", "jordan", "henry", "nick", "tom", "meyer", "george"];
 
 let guest_name = prompt("Inserisca il suo nome");
+let guest_name_lowercase = guest_name.toLocaleLowerCase();
+let guest_name_final = guest_name.charAt(0).toUpperCase() + guest_name.slice(1);
 
-if (guest_list.includes(guest_name)) {
-    console.log("Benvenuto/a, " + guest_name);
+if (guest_list.includes(guest_name_lowercase)) {
+    console.log("Benvenuto/a, " + guest_name_final);
 } else {
     console.log("Ci dispiace, non è sulla lista.");
 };
