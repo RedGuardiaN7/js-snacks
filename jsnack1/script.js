d6fb2075beb495c71a8e6f1881b12699d4068723
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-14 14:23:45 +0100

    Fine jsnack 1 e inizio jsnack 2

---
+++ jsnack1/script.js
@@ -0,0 +1,8 @@
+let StringNumber_1 = prompt("Didgiti il primo numero");
+let StringNumber_2 = prompt("Digiti il secondo numero");
+
+let number_1 = Number(StringNumber_1);
+let number_2 = Number(StringNumber_2);
+let sum = number_1 + number_2
+
+console.log("La somma dei numeri da lei scelti è " + sum)
